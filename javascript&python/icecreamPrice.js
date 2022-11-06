f8c8e82302e063f1dbc9eb9362a96450df7eae47
Violet Ultra <evia_a@naver.com>
2022-11-07 00:31:56 +0900

    javascript 와 python 비교

---
+++ javascript&python/icecreamPrice.js
@@ -0,0 +1,13 @@
+let members = 20;
+let price = 1000;
+let result = members * price;
+
+const icecreamPrice = (members, price) => {
+  return members * price;
+}
+
+// function icecreamPrice(members, price) {
+//   return members * price;
+// }
+
+console.log(icecreamPrice(20, 1000));
